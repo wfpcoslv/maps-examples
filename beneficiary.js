195e5ee92df42d41a4fff8ffca9f9694db8a258f
Mario Gómez <mario.gomez@wfp.org>
2016-12-27 17:56:16 -0600

    Added more examples.

--- beneficiary.js
+++ beneficiary.js
@@ -6,3 +6,11 @@
  */
 BENEFICIARY_GROUP_ID = 1;
 BENEFICIARY_AGE = 11570;
+
+// Other tests (Child under 2):
+//BENEFICIARY_GROUP_ID = 2;
+//BENEFICIARY_AGE = 100;
+
+// Other tests (Child over 2):
+//BENEFICIARY_GROUP_ID = 2;
+//BENEFICIARY_AGE = 721;
