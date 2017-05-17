b6f29f1737d86eb5605b6c0f4bbe3d511c1ef669
Mario Gómez <mario.gomez@wfp.org>
2017-05-17 09:26:33 -0600

    Added standard API directory

---
+++ api/test_data.js
@@ -0,0 +1,41 @@
+// use del usuario actual.
+var userData = {
+  "id": 1,
+  "firstname":"",
+  "lastName": "",
+  "email" : "",
+  "role": ""
+};
+
+var beneficiaryData = {
+  "info" : {
+    "beneficiary_id": 0,
+    "birth_date": "",
+    "expiration_date": null,
+    "registration_date": "",
+    "gender": 0,
+    "id_document": "",
+    "name": "",
+    "first_name": "",
+    "group_id": 0,
+    "nutrimiles_id": "",
+    "version": 0
+    },
+  "events" :
+    [
+      {
+        "type": ,
+        "userID": 0,
+        "date": "",
+        "deviceID": 0,
+        "location": {
+          "latitude": 0,
+          "longitude": 0
+        },
+        "data": {
+	  // fill with data
+        },
+        "validated": null
+      }
+    ]
+};
