b5b546c9705860191a1207d0fed12743fda53e38
Mario Gomez <mario.gomez@wfp.org>
2017-10-04 14:45:04 -0600

    A lot of cleanup. More makefile commands.

---
+++ examples/SV_WFP_MilDias/lang_es.js
@@ -0,0 +1,107 @@
+// Requires lang.js
+var lang_es = new Language();
+lang_es.default_alert =      "Algo malo ha sucedido, contacte al proveedor."
+lang_es.ok =                 "OK"
+lang_es.cancel =             "Cancelar"
+lang_es.weight =             "Peso"
+lang_es.in_weight =          "en Kg"
+lang_es.lenght =             "Longitud"
+lang_es.in_lenght =          "en cm"
+lang_es.length =             "Longitud"
+lang_es.in_length =          "en cm"
+lang_es.height =             "Talla"
+lang_es.in_height =          "en cm"
+lang_es.circum =             "Circunferencia craneana"
+lang_es.in_circum =          "en cm"
+lang_es.add_entry =          "Guardar en tarjeta"
+lang_es.gest_age =           "Edad gestacional"
+lang_es.in_gest_age =        "en semanas"
+lang_es.intra_growth =       "Crecimiento intrauterino"
+lang_es.in_intra_growth =    "en cm"
+lang_es.visit_date =         "Fecha de visita"
+lang_es.age_days =           "Edad días a la visita"
+lang_es.nutritional_status = "Estado nutricional"
+lang_es.scp_given =          "SCP entregado"
+lang_es.in_scp =             "unidades"
+lang_es.is_allowed_to_get =  "Este beneficiario puede recibir "
+lang_es.units_csp =          " unidades de CSP"
+lang_es.not_allowed =         "Este beneficiario no puede recibir más super-cereal."
+lang_es.age =                "Edad"
+lang_es.gestationalAge =     "Edad Gestacional"
+lang_es.intrauterineGrowth = "Crecimiento intrauterino"
+lang_es.headCircumference =  "Circunferencia craneana"
+lang_es.scpReceived =        "SCP entregado"
+lang_es.nextVisit =          "Siguiente visita"
+lang_es.gaveBirth =          "Dió a luz"
+lang_es.childDeath =         "Muerte infantil"
+lang_es.deinf =              "Estimulación del Desarrollo Infantíl"
+lang_es.alimsal =            "Alimentación Saludable"
+lang_es.habvid =             "Habilidades para la vida"
+// Para captura de coresponsabilidad:
+lang_es.tiponac =            "Tipo de nacimiento:"
+lang_es.esquemavac =         "Esquema de vacunación según edad:"
+lang_es.vita =               "Micronutritentes: Vitamina A"
+lang_es.vitsf =              "Micronutrientes: Sulfato Ferroso"
+lang_es.antipar =            "Antiparasitarios: Mebandazole"
+lang_es.anemia =             "Respuesta de examen Hemoglobina"
+lang_es.antitetanica =       "Vacunación Antitetánica"
+lang_es.influenza =          "Vacunación Influenza Estacional"
+lang_es.ggh =                "Vacunación con Gamma Globulina hiperinmune"
+lang_es.enrn =               "Estado Nutricional del RN"
+lang_es.folico =             "Ácido Fólico"
+lang_es.hierro =             "Hierro"
+lang_es.calcio =             "Calcio"
+lang_es.lactan =             "Lactancia Materna"
+lang_es.seq =                "Secuencia de visita"
+lang_es.emboff =             "Edad gestacional"
+lang_es.peso =               "Peso (kg)"
+lang_es.talla =              "Talla (cm)"
+
+// Opciones de selección comunes
+lang_es.SI_NO = [
+    { 'v': 1, 't': "Sí"},
+    { 'v': 2, 't': "No"}
+  ];
+lang_es.SI_NO_OTROS = [
+    { 'v': 1, 't': "Sí"},
+    { 'v': 2, 't': "No (No hay)"},
+    { 'v': 3, 't': "No (No lo toma)"},
+    { 'v': 4, 't': "No (Otro)"}
+];
+lang_es.TIPO_NAC = [
+    { 'v': 1, 't': "Intrahospitalario"},
+    { 'v': 2, 't': "Intradomiciliar"}
+];
+lang_es.RSP_HEMOGLOB = [
+    { 'v': 1, 't': "Hay Anemia"},
+    { 'v': 2, 't': "No hay Anemia"},
+    { 'v': 3, 't': "No se realizó el exámen"},
+];
+lang_es.RSP_ENRN = [
+    { 'v': 1, 't': "Bajo peso"},
+    { 'v': 2, 't': "Normal"},
+    { 'v': 3, 't': "Arriba de lo normal"},
+];
+
+// Asignación de listas de selección
+lang_es.options = [
+  { 'name': 'deinf', 'options': lang_es.SI_NO },
+  { 'name': 'alimsal', 'options': lang_es.SI_NO },
+  { 'name': 'habvid', 'options': lang_es.SI_NO },
+  { 'name': 'tiponac', 'options': lang_es.TIPO_NAC },
+  { 'name': 'esquemavac', 'options': lang_es.SI_NO },
+  { 'name': 'vita', 'options': lang_es.SI_NO_OTROS },
+  { 'name': 'vitsf', 'options': lang_es.SI_NO_OTROS },
+  { 'name': 'antipar', 'options': lang_es.SI_NO_OTROS },
+  { 'name': 'anemia', 'options': lang_es.RSP_HEMOGLOB },
+  { 'name': 'antitetanica', 'options': lang_es.SI_NO },
+  { 'name': 'influenza', 'options': lang_es.SI_NO },
+  { 'name': 'ggh', 'options': lang_es.SI_NO },
+  { 'name': 'enrn', 'options': lang_es.RSP_ENRN },
+  { 'name': 'folico', 'options': lang_es.SI_NO_OTROS },
+  { 'name': 'hierro', 'options': lang_es.SI_NO_OTROS },
+  { 'name': 'calcio', 'options': lang_es.SI_NO_OTROS },
+  { 'name': 'lactan', 'options': lang_es.SI_NO_OTROS }
+];
+
+lang = lang_es;
--- examples/SV_WFP_MilDias/test_data.js
+++ examples/SV_WFP_MilDias/test_data.js
@@ -1,37 +1,19 @@
 // userData contiene los datos del usuario actual.
 var userData = {
   "id": 1,
-  "firstname":"MONITOR",
+  "firstname":"MEDICO",
   "lastName": "Salamanca",
   "email" : "isabella@salamanca.com",
   "role": "Health Worker"
 };
-/* Otros ejemplos:
-// Tienda
-var userData = {
-  "id": 1,
-  "firstname":"TIENDA",
-  "lastName": "UNO",
-  "email" : "tienda1@maps.org",
-  "role": "Health Worker"
-};
-// Monitor de campo
-var userData = {
-  "id": 1,
-  "firstname":"MONITOR",
-  "lastName": "UNO",
-  "email" : "tienda1@maps.org",
-  "role": "Field Monitor"
-};
- */
 
 var beneficiaryData = {
   "info" : {
     "beneficiary_id": 65789641025451,
-    "birth_date": "2016-09-20",
+    "birth_date": "2017-04-03",
     "expiration_date": null,
     "registration_date": "2015-01-02",
-    "gender": 2,
+    "gender": 1,
     "id_document": "45547150048948541",
     "name": "Villatoro",
     "first_name": "Claudia",
@@ -44,81 +26,96 @@ var beneficiaryData = {
       {
         "type": 1,
         "userID": 1,
-        "date": "2017-09-20T13:33:40.919Z",
+        "date": "2017-04-10 00:00:00",
         "deviceID": 112233,
         "location": {
           "latitude": 1,
           "longitude": 1
         },
         "data": {
-          "training": 1,
-          "gaveBirth" : 1,
-          "childDeath" : 1,
-	  "preg_offset" : 1
+          "seq": 1,
+          "peso": 2.95,
+          "talla": 49,
+	  "tiponac": 1,
+	  "enrn": 2,
+	  "lactan": 1,
+	  "esquemavac": 1,
+	  "edad_dias": 7
         },
         "validated": true
       },
       {
         "type": 1,
         "userID": 1,
-        "date": "2016-11-15T13:33:40.919Z",
+        "date": "2017-05-02 00:00:00",
         "deviceID": 112233,
         "location": {
           "latitude": 1,
           "longitude": 1
         },
         "data": {
-          "weight": 3.7,
-          "height": 52,
-          "age": 16,
-          "gestationalAge" : 23,
-          "intrauterineGrowth" : 10,
-          "headCircumference" : 20,
-          "length" : 52,
-          "scpReceived" : 0,
-          "nextVisit" : "2017-01-01"
+          "seq": 2,
+          "peso": 2.85,
+          "talla": 49,
+	  "lactan": 1,
+	  "esquemavac": 1,
+	  "edad_dias": 29
         },
         "validated": true
-      }, {
+      },
+      {
         "type": 1,
-        "userID": 2,
-        "date": "2016-11-15T13:33:40.926Z",
+        "userID": 1,
+        "date": "2017-06-05 00:00:00",
         "deviceID": 112233,
         "location": {
-          "latitude": 2,
-          "longitude": 2
+          "latitude": 1,
+          "longitude": 1
         },
         "data": {
-          "weight": 7.0,
-          "height": 56,
-          "age": 111,
-          "gestationalAge" : 23,
-          "intrauterineGrowth" : 10,
-          "headCircumference" : 20,
-          "length" : 56,
-          "scpReceived" : 0,
-          "nextVisit" : "2017-01-01"
+          "seq": 3,
+          "peso": 3.3,
+          "talla": 50,
+	  "esquemavac": 1,
+	  "edad_dias": 63
         },
         "validated": true
-      }, {
+      },
+      {
         "type": 1,
-        "userID": 2,
-        "date": "2016-11-19T13:33:40.926Z",
+        "userID": 1,
+        "date": "2017-08-06 00:00:00",
         "deviceID": 112233,
         "location": {
-          "latitude": 2,
-          "longitude": 2
+          "latitude": 1,
+          "longitude": 1
         },
         "data": {
-          "weight": 0,
-          "age": 0,
-          "height": 0,
-          "headCircumference" : 0,
-          "gestationalAge" : 0,
-          "intrauterineGrowth" : 0,
-          "length" : 0,
-          "scpReceived" : 5,
-          "nextVisit" : "2017-01-01"
+          "seq": 4,
+          "peso": 4.8,
+          "talla": 53,
+	  "esquemavac": 1,
+	  "edad_dias": 63
+        },
+        "validated": true
+      },
+      {
+        "type": 1,
+        "userID": 1,
+        "date": "2017-10-04 00:00:00",
+        "deviceID": 112233,
+        "location": {
+          "latitude": 1,
+          "longitude": 1
+        },
+        "data": {
+          "seq": 5,
+          "peso": 5,
+          "talla": 58,
+	  "esquemavac": 1,
+	  "vita": 2,
+	  "vitsf": 1,
+	  "edad_dias": 63
         },
         "validated": true
       }
